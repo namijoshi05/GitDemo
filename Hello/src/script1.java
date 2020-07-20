94088bb156d1a116a87b38d83a92a74f8a70b472
Namita <namijoshi05@gmail.com>
2020-07-20 17:24:42 +0200

    Updated code1&2

--- Hello/src/script1.java
+++ Hello/src/script1.java
@@ -3,6 +3,9 @@ public class script1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello There is an update1 in Git");
+		System.out.println("Hello There is an update2 in Git");
+		
 
 	}
 
